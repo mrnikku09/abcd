db263f8abb3f98bce7c1d8174749380026b3e2c5
mrnikku09 <115152182+Nitesh069@users.noreply.github.com>
2023-11-26 22:26:23 +0530

    sjb

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>jhgj</h1>
+      <button className='btn btn-primary'>daba de</button>
+    </>
+  );
+}
+
+export default App;
